98150cd193b6bfe3e08ca8140baeb2600399d7a2
ALIEN <matthias.moulin@gmail.com>
2017-03-20 15:51:43 +0100

    Formatting

--- cu-smallpt/src/kernel.cu
+++ cu-smallpt/src/kernel.cu
@@ -16,7 +16,7 @@ Sphere spheres[] = {
 	Sphere(1e5,  Vector3(50, 40.8, 1e5),		 Vector3(),   Vector3(0.75), DIFFUSE),			   //Back
 	Sphere(1e5,  Vector3(50, 40.8, -1e5 + 170),  Vector3(),   Vector3(), DIFFUSE),				   //Front
 	Sphere(1e5,  Vector3(50, 1e5, 81.6),		 Vector3(),   Vector3(0.75), DIFFUSE),			   //Bottom
-	Sphere(1e5,  Vector3(50, -1e5 + 81.6, 81.6), Vector3(),   Vector3(0.75), DIFFUSE),			   //Top
+	Sphere(1e5,  Vector3(50, -1e5 + 81.6, 81.6), Vector3(),   Vector3(0.75), DIFFUSE),			   //Top																					   
 	Sphere(16.5, Vector3(27, 16.5, 47),			 Vector3(),   Vector3(0.999), SPECULAR),	       //Mirror
 	Sphere(16.5, Vector3(73, 16.5, 78),			 Vector3(),	  Vector3(0.999), REFRACTIVE),	       //Glass
 	Sphere(600,	 Vector3(50, 681.6 - .27, 81.6), Vector3(12), Vector3(), DIFFUSE)				   //Light
